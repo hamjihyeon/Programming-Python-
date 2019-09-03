ab2ae7fce43a2707352935b9ee8291a483558872
jihyeon <gkawlgus01@naver.com>
2019-09-03 11:00:58 +0900

    add mathmodule

---
+++ mathmodule.py
@@ -0,0 +1,33 @@
+import math
+
+print("파이 상수")
+print("math.pi ", math.pi)
+print()
+print("e 상수")
+print("math.e :", math.e)
+print()
+print("올림 - 3.3의 올림")
+print("math.ceil(3,5) :", math.ceil(3.5))
+print()
+print("내림 - 3.5의 내림")
+print("math.floor(3.5) :", math.floor(3.5))
+print()
+print("계승 구하기 - 5!(5계승)")
+print("math.factorial(5) :", math.factorial(5))
+print()
+print("최대공약구 구하기 - 28과 70의 최대공약수")
+print("math.gcd(28,70) :", math.gcd(28,70))
+print()
+print("거듭제곱 구하기 - 2의 10승 (2*10)")
+print("math.pow(2,10) :", math.pow(2, 10))
+print()
+
+print("삼각함수 - sin(1/2 π) :", math.sin(math.pi/2))
+print()
+
+print("삼각함수 - cos(π) :", math.cos(math.pi))
+print()
+
+print("삼각함수 - tan(0) :", math.cos(math.tan(0)))
+print()
+
