1494571f64189e9921e07160ef858683fa79f1a4
jihyeon <gkawlgus01@naver.com>
2019-10-08 11:07:43 +0900

    write text(ama) file, read text(ama) file, readline, 가격의 총합

--- VII. 파일 처리/read_text_file2.py
+++ VII. 파일 처리/read_text_file2.py
@@ -1,12 +1,16 @@
 fi = open("history.ama", "r", encoding="utf-8")
+
+sum = 0
 while True:
     data = fi.readline()
     if not data:
         break
-    print(data, end = "")
+    # print(data, end = "")
     # 한줄 안에서 이름 가격 ... 자르기
+    l = data.split()    #화이트스페이스 \t, \n 뛰어쓰기 있는걸 다 지움
+    sum += int(l[1])    #음료의 가격 합계를 구한다.
     # 총금액 계산하고 출력하기
-
+print("총금액 : " + str(sum) + "원")
 fi.close()
 
 #print(data)
--- VII. 파일 처리/write_text_file4.py
+++ VII. 파일 처리/write_text_file4.py
@@ -1,7 +1,7 @@
 fi = open("history.ama", "w", encoding="utf-8")
 
-fi.write("아메리카노\t2,300원\t레귤러\t50%\t100%\t코코\n")
-fi.write("타로버블티\t3,200원\t점보\t100%\t100%\t타피오카\n")
-fi.write("티라미수버블티\t3,800원\t점보\t0%\t100%\t타피오카\n")
+fi.write("아메리카노\t2300\t0\t1\t2\t1\n")
+fi.write("타로버블티\t3200\t1\t2\t2\t0\n")
+fi.write("티라미수버블티\t3800\t1\t0\t2\t0\n")
 
 fi.close()
