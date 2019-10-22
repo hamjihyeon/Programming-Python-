482e2fede0b10c154c47951a3316de1b7b02c085
jihyeon <gkawlgus01@naver.com>
2019-10-22 09:37:07 +0900

    save,load 아마스빈 history

---
+++ 1학기/amasvin/main.py
@@ -0,0 +1,23 @@
+#main.py
+from order import Order
+from file_manager import FileManager
+
+#주문내역 불러오고 보여주자
+file_manager = FileManager("history.bin")
+# answer = input("주문내역을 볼까요?(y or n)")
+# if answer == "y":
+history = []
+try:
+    history = file_manager.load()
+    sum = 0
+    for h in history:
+        print(h)
+        sum += h.price
+    print("여태까지 내가 아마스빈에 쏟아부은 돈 : " + str(sum) + "원")
+except FileNotFoundError:
+    print("주문 내역이 없습니다.")
+o = Order()
+o.order_drink()
+
+#주문내역 저장하자
+file_manager.save(history + o.order_menu)
