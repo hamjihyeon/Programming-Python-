c07d5e314ba2dd6c3e350f72e3a5e7e6c24c4f23
jihyeon <gkawlgus01@naver.com>
2019-11-05 10:38:40 +0900

    tictactoe , GameManager GUi 완성

--- IX.프로젝트/game_manager.py
+++ IX.프로젝트/game_manager.py
@@ -1,4 +1,7 @@
 from tictactoe import TicTacToe
+import tkinter
+from tkinter import messagebox
+import math
 
 class GameManager:
     def __init__(self):
@@ -27,8 +30,64 @@ class GameManager:
                 print("무승부")
                 break
 
+class GameManager_GUI:
+    def __init__(self):
+        self.ttt = TicTacToe()
+        CAMBUS_SIZE = 300
+        self.TILE_SIZE = CAMBUS_SIZE/3
+
+        self.root = tkinter.Tk()
+        self.root.title("틱 택 토")
+        self.root.geometry(str(CAMBUS_SIZE)+"x"+str(CAMBUS_SIZE)) # geometry("300x300")
+        self.root.resizable(width = False, height = False)
+        self.canvas = tkinter.Canvas(self.root, bg = "white", width = CAMBUS_SIZE, height = CAMBUS_SIZE)
+
+        self.canvas.pack() #필수
+
+        self.images = dict()
+        self.images["O"] = tkinter.PhotoImage(file = "img/O.gif")
+        self.images["X"] = tkinter.PhotoImage(file = "img/X.gif")
+        self.canvas.bind("<Button-1>", self.click_handler)
+
+    def click_handler(self, event):
+        row = math.floor(event.y/self.TILE_SIZE)
+        col = math.floor(event.x/self.TILE_SIZE)
+        self.ttt.set(row,col)
+        self.draw_board()
+        if self.ttt.check_winner() == "O":
+            messagebox.showinfo("게임 오버", "O win!!!")
+            self.root.quit()
+        elif self.ttt.check_winner() == "X":
+            messagebox.showinfo("게임 오버", "X win!!!")
+            self.root.quit()
+        elif self.ttt.check_winner() == "d":
+            messagebox.showinfo("게임 오버", "무승부!!!")
+            self.root.quit()
+
+
+    def draw_board(self):
+        self.canvas.delete("all")
+
+        x = 0
+        y = 0
+        for i,v in enumerate(self.ttt.board):
+            if v == ".":
+                pass
+            elif v == "O":
+                self.canvas.create_image(x,y,anchor = "nw", image = self.images["O"])
+            elif v == "X":
+                self.canvas.create_image(x,y,anchor = "nw", image = self.images["X"])
+            x += self.TILE_SIZE
+            if i % 3 == 2:
+                x = 0
+                y += self.TILE_SIZE
+
+
+    def play(self):
+        self.root.mainloop()
+
 
 
 if __name__ == '__main__':
-    gm = GameManager()
+    gm = GameManager_GUI()
     gm.play()
